ad12670255bee2dcc67c86d60b9de2eb8aa077a9
karnthis <erin@atomicbear.com>
2018-01-13 22:28:35 -0600

    Final working version, users/db/table names changed to placeholders

--- index.js
+++ index.js
@@ -1,32 +1,77 @@
-var MySQLEvents = require('mysql-events');
-var dsn = {
+const MySQLEvents = require('mysql-events');
+const mysql = require('mysql');
+
+const eventCred = {
   host:     'localhost',
-  user:     'root',
-  password: '' // no password set that's why keep blank
+  user:     'eventUser',
+  password: ''
 };
-var mysqlEventWatcher = MySQLEvents(dsn);
-console.log(mysqlEventWatcher);
-var watcher =mysqlEventWatcher.add(
-   'mc2',
+const sqlCred = {
+  host:     'localhost',
+  user:     'deletionUser',
+  password: '',
+  database: 'dbName'
+};
+
+const mysqlConn = mysql.createConnection(sqlCred);
+const mysqlEventWatcher = MySQLEvents(eventCred);
+
+const watcher = mysqlEventWatcher.add(
+  'dbName.gpTableName',
   function (oldRow, newRow, event) {
-     //row inserted
+    let rowObj = event.rows[0];
+    //row inserted
     if (oldRow === null) {
       //insert code goes here
-      console.log(event);
+      //console.log(rowObj);
+      let dim = rowObj.lessercorner.split(";")[0];
+      switch (dim) {
+        case "world":
+        console.log("Permitted dim");
+        break;
+        case "DIM-1":
+        console.log("Permitted dim");
+        break;
+        case "DIM1":
+        console.log("Permitted dim");
+        break;
+        case "DIM6":
+        console.log("Permitted dim");
+        break;
+        case "DIM7":
+        console.log("Permitted dim");
+        break;
+        case "DIM-37":
+        console.log("Permitted dim");
+        break;
+        case "DIM-42":
+        console.log("Permitted dim");
+        break;
+        case "DIM-112":
+        console.log("Permitted dim");
+        break;
+        default:
+        console.log("Not permitted dim. Purging record: "+rowObj.id);
+        let sql = "DELETE FROM griefprevention_claimdata WHERE id = "+rowObj.id;
+        console.log("Purging...");
+        mysqlConn.query(sql, function () {
+          console.log("Purge Complete");
+        });
+      }
     }
-
-     //row deleted
+/*
+    //row deleted
     if (newRow === null) {
       //delete code goes here
     }
 
-     //row updated
+    //row updated
     if (oldRow !== null && newRow !== null) {
       //update code goes here
     }
 
     //detailed event information
     //console.log(event); // don't matter, it updates, delete or insert
-  },
-  'Active'
+*/
+  }
 );
